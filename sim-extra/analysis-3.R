f7dccab5eb36bb6826150be699b8144bc0280542
Samuel Perreault <samuel.perreault.3@ulaval.ca>
2021-01-14 09:26:13 -0500

    deletions taken care of

--- sim-extra/analysis-3.R
+++
@@ -1,4 +0,0 @@
-pvals <- readRDS("pvals_extra_3.rds")
-
-par(mfrow=c(1,1), mar=c(2,2,1,1))
-plot(seq_along(pvals)/length(pvals),sort(pvals),ylim=c(0,1))
--- sim-main/functionsLowBlock2/designToClus.R
+++
@@ -1,4 +0,0 @@
-designToClus <- function(d,design){
-  if(design == "equi") return(rep(1,each=d))
-  if(design == "block") return(rep(1:5,each=d/5))
-}
