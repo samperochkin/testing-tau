d6a71db94ba22435cd1d928fa135e0f035a6fc5e
samperochkin <samuel.perreault.3@ulaval.ca>
2021-07-28 00:14:46 +0000

    complete simulation results cc take 3 -- removed block

--- sim/sim-main/analysisScripts/aggregate-results-cc.R
+++ sim/sim-main/analysisScripts/aggregate-results-cc.R
@@ -2,6 +2,7 @@ library(data.table)
 source("sim/sim-main/analysisScripts/tables/functions/constructDT.R")
 
 fii <- list.files("/store/samuel/testing-tau-extra",full.names = T)
+fii <- fii[!grepl("block", fii)]
 fii <- fii[!grepl("boot_1", fii)]
 fii <- fii[!grepl("boot_2", fii)]
 fii <- fii[!grepl("low_3", fii)]
