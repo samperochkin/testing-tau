ed09968736cc4d2b01251e44faf0a385d2f97911
Samuel Perreault <samuel.perreault.3@ulaval.ca>
2021-09-22 21:14:25 -0400

    analysis 500 and sim plots

---
+++ sim/sim-main/analysisScripts/extra-500/analysis500-check-with-other.R
@@ -0,0 +1,64 @@
+# packages ----------------------------------------------------------------
+library(data.table)
+library(ggplot2)
+library(latex2exp)
+
+
+# setup -------------------------------------------------------------------
+
+# load appropriate file
+fii <- list.files("sim/sim-main/results/results-extra-500/",full.names = T)
+fii
+dt <- fread(fii[grepl("500_1", fii)])
+dt <- dt[, .(rejection_rate = mean(pvalue < .05)*100), .(n,d,tau,dtau,dtau_type,S,Sh,norm,distribution)]
+
+
+# set some variables to factors for ggplot
+dt$dtau_type <- factor(dt$dtau_type, level = c("none","single","column"))
+dt$dtau_type <- factor(dt$dtau_type,levels = c("none","single","column"))
+dt$S <- factor(dt$S,levels = c("Sh","I","Sh-p","Sh-d"))
+dt <- dt[S %in% c("Sh","I")] # restrict choice of S
+dt$Sh <- factor(dt$Sh,levels = c("ShP","ShJ","SbP","SbJ"))
+dt$norm <- factor(dt$norm)
+dt$distribution <- factor(dt$distribution)
+
+
+
+
+ggplot(dt, aes(x=d, y=rejection_rate, shape=distribution, linetype=norm)) +
+  theme_light() +
+  theme(panel.grid.minor = element_blank(),
+        strip.background = element_rect(fill="gray95"),
+        strip.text = element_text(colour = 'black')) +
+  geom_point(size=1.25) +
+  geom_line() +
+  xlab("dimension (d)") +
+  ylab("rejection rate") +
+  # scale_x_continuous(breaks = c(5, 15, 25, 50, 100)) +
+  # scale_linetype_manual(name = "norm (statistic)", breaks = c("Euclidean", "Supremum"), values=c(1:2),
+  #                       labels = c(expression(paste("Euclidean (", E["np"], ")")), expression(paste("Supremum (", M["np"], ")")))) +
+  # scale_shape_manual(name = "copula", breaks = c("normal", "t4", "gumbel", "clayton"), values = c(16,15,17,3), labels = c("Normal", expression(paste("student's ", t[4])), "Gumbel", "Clayton")) +
+  facet_grid(S~dtau_type, labeller = label_bquote(cols = paste(.(paste0(dtau_type)), " departure")))
+
+
+ggplot(dt[distribution == "normal"],
+       aes(x=d, y=rejection_rate, shape=S, linetype=norm)) +
+  ggtitle(TeX("Power of the tests for exchangeability (H_0^*, Normal copula)")) +
+  theme_light() +
+  theme(panel.grid.minor = element_blank(),
+        strip.background = element_rect(fill="gray95"),
+        strip.text = element_text(colour = 'black'),
+        legend.text = element_text(size = 11)) +
+  geom_point(size=2) +
+  geom_line() +
+  xlab("dimension (d)") +
+  ylab("rejection rate") +
+  scale_x_continuous(breaks = c(5, 15, 25, 50, 100)) +
+  scale_linetype_manual(name = "norm (statistic)", breaks = c("Euclidean", "Supremum"), values=c(1,2),
+                        labels = c(expression(paste("Euclidean (", E["np"], ")")), expression(paste("Supremum (", M["np"], ")")))) +
+  scale_shape_manual(breaks = c("I", "Sh"), values = c(16,15),
+                     labels = c(expression(paste("(1/n)", I[p])), expression(hat(Sigma)[np]))) +
+  geom_hline(yintercept=.05, lty=3, col="gray25") +
+  facet_grid(~dtau_type, labeller = label_bquote(cols = paste(.(paste0(dtau_type)), " departure")))
+
+
